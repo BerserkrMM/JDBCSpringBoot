0e037aaea6e1e76eab53da359ab17b0e3be70e8b
BerserkrMM <mr.monopoly.od@gmail.com>
2022-12-30 14:28:24 +0200

    feat: Added configuration for testContainer

---
+++ src/test/java/com/example/jdbcspringbootapp/config/TestContainersConfig.java
@@ -0,0 +1,42 @@
+package com.example.jdbcspringbootapp.config;
+
+import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+public class TestContainersConfig {
+    public static class CustomMSSQLContainer extends MSSQLServerContainer<CustomMSSQLContainer> {
+
+        private static final String IMAGE_VERSION = "mcr.microsoft.com/mssql/server:2017-latest";
+
+        private static CustomMSSQLContainer container;
+
+        public CustomMSSQLContainer() {
+            super(IMAGE_VERSION);
+        }
+
+        public static CustomMSSQLContainer getInstance() {
+            if (container == null) {
+                container = new CustomMSSQLContainer()
+                        .acceptLicense()
+                        .withInitScript("sql/create_schema.sql")
+                        .waitingFor(Wait.forListeningPort());
+            }
+            return container;
+        }
+
+        @Override
+        public void start() {
+            super.start();
+            System.setProperty("spring.datasource.driver-class-name", container.getDriverClassName());
+            System.setProperty("spring.datasource.url", container.getJdbcUrl());
+            System.setProperty("spring.datasource.username", container.getUsername());
+            System.setProperty("spring.datasource.password", container.getPassword());
+        }
+
+        @Override
+        public void stop() {
+            //do nothing, JVM handles shut down
+        }
+
+    }
+}
